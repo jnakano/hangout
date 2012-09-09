f227e7f4b9e95cdf64b1ac47d02e3e83308a258a
Cameron Ellis <cameronellis.ellis@gmail.com>
2012-09-08 20:12:32 -0700

    added jinja2 and index.html template

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
+
+assets_path = os.path.join(os.path.dirname(__file__), 'static')
+
+jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(assets_path))
+
+class User(db.Expando):
+  location = db.ListProperty(float)
+  email = db.StringProperty()
+  event = db.StringProperty()
+  name = db.StringProperty()
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+  def get(self):
+    template = jinja_environment.get_template("index.html")
+    self.response.out.write(template.render({
+        "moon":55
+      }))
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
-                              debug=True)
+class LocationHandler(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write("your location is -80,-80")
+
+app = webapp2.WSGIApplication(
+  [('/', MainHandler),
+    ('/lh', LocationHandler)
+    ], debug = True)
