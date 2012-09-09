531f0f9d52f82b8e7e50c75c105155105bd7331f
moonlimb <moonlimb@gmail.com>
2012-09-09 07:14:22 -0700

    add two html pages

---
+++ hangout/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import webapp2
+import jinja2
+from google.appengine.ext import db
+
+assets_path = os.path.join(os.path.dirname(__file__), 'static')
+
+jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(assets_path))
+
+class User(db.Expando):
+  location = db.ListProperty(float)
+  email = db.StringProperty()
+  event = db.StringProperty()
+  name = db.StringProperty()
+
+class BaseHandler(webapp2.RequestHandler):
+  def get(self):
+    pass
+
+
+class MainHandler(webapp2.RequestHandler):
+  def get(self):
+    template = jinja_environment.get_template("index.html")
+    self.response.out.write(template.render({
+        "moon":55
+      }))
+
+class LocationHandler(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write("your location is -80,-80")
+
+app = webapp2.WSGIApplication(
+  [('/', MainHandler),
+    ('/lh', LocationHandler)
+    ], debug = True)
